b1dc2859f0bf7928730862f6cd8ccc92d1fa06fd
Aaron Louie <ajlouie@gmail.com>
2020-08-18 13:12:54 -0400

    Adds interface for building locations. Organizes files into assets, data, components, and interfaces directories.

---
+++ components/Styles.tsx
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native';
+
+
+export const Styles = StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      backgroundColor: '#202741',
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 20,
+    },
+    iconText: {
+      fontSize: 25,
+      fontStyle: 'italic',
+      color: 'white'
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      height: '7%',
+      width: '100%'
+    },
+    // Really confused why the images seem to go out of the 
+    // container with ie. alignSelf: 'flex-start'
+    sabreLogo: {
+      resizeMode: "contain",
+      width: '10%'
+    },
+    seperator: {
+      height: '1%'
+    },
+    notchSeperator: {
+      height: '5%'
+    },
+    mapContainer: {
+      height: '34%',
+      width: '95%',
+      borderRadius: 20,
+      backgroundColor: 'white',
+    },
+    list: {
+      height: '54%',
+      width: '95%',
+    },
+    listAccordion: {
+      backgroundColor: 'white',
+      backfaceVisibility: 'hidden',
+      borderRadius: 10,
+      marginTop: 4,
+    },
+    listItem: {
+      backgroundColor: 'white',
+      zIndex: -1,
+      marginTop: -10,
+      borderRadius: 10,
+      borderTopRightRadius: 0,
+      borderTopLeftRadius: 0,
+      marginBottom: 1,
+    }
+  });
+  
---
+++ data/BuildingLocations.tsx
@@ -0,0 +1,77 @@
+import { BuildingLocation } from '../interfaces/BuildingLocation';
+
+
+export const BuildingLocations: BuildingLocation[] = [
+  {
+    id: 1,
+    name: "Alderman",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 2,
+    name: "Brown",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 3,
+    name: "Clemons",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 4,
+    name: "1515",
+    capacity: 100,
+    occupancy: 80,
+  },
+  {
+    id: 5,
+    name: "Newcomb",
+    capacity: 700,
+    occupancy: 80,
+  },
+  {
+    id: 6,
+    name: "Rotunda",
+    capacity: 300,
+    occupancy: 80,
+  },
+  {
+    id: 7,
+    name: "Rice Hall",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 8,
+    name: "Music Library",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 9,
+    name: "Fine Arts Library",
+    capacity: 500,
+    occupancy: 80,
+  },
+  {
+    id: 10,
+    name: "Grit Coffee",
+    capacity: 100,
+    occupancy: 80,
+  },
+  {
+    id: 11,
+    name: "New Cabell Hall",
+    capacity: 700,
+    occupancy: 80,
+  },
+  {
+    id: 12,
+    name: "Starbucks",
+    capacity: 300,
+    occupancy: 80,
+  }
+]
