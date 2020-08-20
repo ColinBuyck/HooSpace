eb81dcfe2748674756e59f0c69b29985991825fd
Colin Buyck <colinbuyck@Colins-MBP-2.lan>
2020-08-20 00:29:15 -0400

    Added capacity percentage graphics and function that auto-adjusts graphic color

--- components/LocationLIst.tsx
+++ components/LocationLIst.tsx
@@ -1,7 +1,10 @@
 import { BuildingLocations } from "../data/BuildingLocations"
 import { List } from "react-native-paper"
 import { Styles } from "./Styles"
-
+import ProgressCircle from "react-native-progress-circle"
+import React from 'react'
+import { Text, processColor } from 'react-native'
+import {progressGraphicColor} from './ProgressGraphicColor'
 
 const listLocations = () => {
     return BuildingLocations.map((item, idx) => {
@@ -11,6 +14,15 @@ const listLocations = () => {
                 id={item.id}
                 style={Styles.listAccordion}
                 theme={{ colors: { primary: 'black', backdrop: 'white' }, animation: { scale: 0 } }}
+                left = {props => 
+                    <ProgressCircle
+                        percent={(item.occupancy/item.capacity)*100}
+                        radius={30}
+                        borderWidth={10}
+                        color = {progressGraphicColor(item.occupancy, item.capacity)}
+                    >
+                        <Text style={{ fontSize: 14 }}>{Math.round((item.occupancy/item.capacity)*100) + '%'}</Text>
+                    </ProgressCircle>}
             >
                 <List.Item title={"Current Capacity: " + item.occupancy + "/" + item.capacity} style={Styles.listItem}>
                 </List.Item>
--- components/ProgressGraphic.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-
-//Occupancy percentage graphic to be used on ccordion headers
-export const ProgressGraphic = (occupancy: number, capacity: number) => {
-    return (
-        <View>
-        </View>
-    );
-}
