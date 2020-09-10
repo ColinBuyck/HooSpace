c33539e9187233338cfa60aa26eaa83662676542
Aaron Louie <ajlouie@gmail.com>
2020-09-10 16:29:17 -0400

    For George

--- data/PullLocations.tsx
+++ data/PullLocations.tsx
@@ -4,16 +4,32 @@ import LocationList from '../components/LocationLIst';
 
 export function PullLocations(): Promise<Location[]> {
     return fetch("http://uvalib-api-occupancy.firebaseio.com/locations-schemaorg/location.json")
-        .then(
-            value => value.json()
-        )
+        .then(value => {
+          console.log('value', value);
+          return value.json();
+        })
         .then(
             json => {
                 var locs = new Array();
                 //console.log("in pulllocations " + JSON.stringify(json));
                 const buildings = ['physics', 'special-collections', "scholars-lab", 'ivy', "music", "main", "astronomy", "mathematics", "science", "health-sciences", "law", "clemons", "fine-arts", "robertson-media-center", "darden", "jag", "harrison"];
-                buildings.forEach(building => {
-                    locs.push({name: json[`${building}.name`], latitude: json[`${building}.latitude`], longitude: json[`${building}.longitude`], isOpenNow: json[`${building}.isOpenNow`], maximumAtendeeCapacity: json[`${building}.maximumAtendeeCapacity`], occupancy: { timestamp: json[`${building}.occupancy.timestamp_end`], count: json[`${building}.occupancy.value`]}, noMaskCount: {timestamp: json[`${building}.noMaskCount.timestamp_end`], count: json[`${building}.noMaskCount.value`]}});
+
+                buildings.forEach((buildingName: string) => {
+                  const building = json[buildingName];
+                  console.log(building['name']);
+                  locs.push({
+                    name: building['name'],
+                    latitude: building['latitude'],
+                    longitude: building['longitude'],
+                    isOpenNow: building['isOpenNow'],
+                    maximumAtendeeCapacity: building['maximumAtendeeCapacity'],
+                    occupancy: { timestamp: building['occupancy.timestamp_end'],
+                    count: building['occupancy.value']},
+                    noMaskCount: {
+                      timestamp: building['noMaskCount.timestamp_end'],
+                      count: building['noMaskCount.value']
+                    }
+                  });
                 });
                 return locs;
             }
