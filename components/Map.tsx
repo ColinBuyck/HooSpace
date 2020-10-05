586cad6ea516ce5884d261fff388485cce6d51ee
Maria Parnell <map5ed@virginia.edu>
2020-10-04 20:21:07 -0400

    pins read from backend, custom markers, more backend checks

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import { Styles } from "./Styles"
 import MapView, { Marker } from 'react-native-maps';
 
 
-const Map = ({locations}: any) => {
+
+const Map = ({ locations }: any) => {
+  // const [center, setCenter] = React.useState({
+  //   latitude: 38.0336,
+  //   longitude: -78.5080,
+  //   latitudeDelta: 0.006,
+  //   longitudeDelta: 0.0003
+  // });
   return (
     <View style={Styles.insidemap}>
       <MapView
@@ -18,60 +25,40 @@ const Map = ({locations}: any) => {
           latitudeDelta: 0.006,
           longitudeDelta: 0.0003
         }}
+        //region={ center }
       >
 
         
         {locations && locations.map((item: any, idx: any) => {
-            if (item && idx && (item.maximumAttendeeCapacity || item['@type'] == "CovidTestSite")) {
+            if (item && idx && item.geo && item.name && (item.maximumAttendeeCapacity || item['@type'] == "CovidTestSite")) {
               return (
                   <Marker
-                    key = {item.name}
+                    key = {idx}
                     coordinate={
                       { latitude: item.geo.latitude, longitude: item.geo.longitude }
                     }
                     title={item.name}
+                    image={require('../assets/hoospace_transparent_small.png')}
                   />
               )
             }
           })
         }
-
-
-{/* <Marker
-                    coordinate={
-                      { latitude: 38.0365, longitude: -78.506099 }
-                    }
-                    title={"Clemons Library"}
-                  />
-        <Marker
-          coordinate={
-            { latitude: 38.0331, longitude: -78.507999 }
-          }
-          title={"Charles L Brown Science & Engineering Library"}
-        />
-        <Marker
-          coordinate={
-            { latitude: 38.035588, longitude: -78.506919 }
-          }
-          title={"Newcomb Hall South Meeting Room Covid Test Site"}
-        />
-        <Marker
-          coordinate={
-            { latitude: 38.035579, longitude: -78.503389 }
-          }
-          title={"Rotunda Multipurpose Room Covid Test Site"}
-        /> */}
-
-        {/* {data.map((item, idx) => (
-          <Marker
-            coordinate={{ latitude: 38.0336, longitude: -78.5080 }}
-          />
-        ))} */}
-
       </MapView>
     </View>
   );
 }
 
+// export const changeCenter = (geo: any) => {
+//   setCenter({
+//     latitude: geo.latitude,
+//     longitude: geo.longitude,
+//     latitudeDelta: 0.006,
+//     longitudeDelta: 0.0003
+//   })
+// }
 
 export default Map
+
+
+//<Image style={Styles.sabreLogo} source={require('../assets/uvaLogo.png')}></Image>
